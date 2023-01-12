71265693e10335bf13b000975d961c6041a7768c
wusub.shin@softcamp.co.kr <wusub.shin@softcamp.co.kr>
2023-01-12 17:14:04 +0900

    first commit

---
+++ app/src/main/java/gradle/simple/versioning/SemanticVersionManager.java
@@ -0,0 +1,10 @@
+package gradle.simple.versioning;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class SemanticVersionManager implements Plugin<Project> {
+    public void apply(Project project) {
+        System.out.println("Hello, World!");
+    }
+}
